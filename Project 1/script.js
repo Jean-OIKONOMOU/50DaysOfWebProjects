ef35487f0f21faddb2a9c4c05f4916d9aacb7d3d
Jean-OIKONOMOU <jean.oikonomou@gmail.com>
2020-12-26 23:30:45 +0100

    project is done

--- Project 1/script.js
+++ Project 1/script.js
@@ -1,3 +1,4 @@
+// getting cat images with AJAX request
 function ajax_get(url, callback) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
@@ -19,14 +20,35 @@ function ajax_get(url, callback) {
 
 let numberOfChildren =  document.querySelector('.container').childElementCount;
 
-    ajax_get('https://api.thecatapi.com/v1/images/search?size=full', function(data) {
-        for (let index = 2; index <= numberOfChildren; index++) {
-            let container = document.querySelector(`.panel:nth-of-type(${index})`);
-            container.style.backgroundImage = `url(${data[0]["url"]})`;
-        }
-    });
-
-    for (let index = 1; index <= numberOfChildren; index++) {
-        let title = document.querySelector(`.panel:nth-of-type(${index}) > h3`);
-        title.textContent = `Panel ${index}`;
+ajax_get('https://api.thecatapi.com/v1/images/search?size=full', function(data) {
+    for (let index = 2; index <= numberOfChildren; index++) {
+        let container = document.querySelector(`.panel:nth-of-type(${index})`);
+        container.style.backgroundImage = `url(${data[0]["url"]})`;
     }
+});
+
+
+// generating a numbered title for each panel
+for (let index = 1; index <= numberOfChildren; index++) {
+    let title = document.querySelector(`.panel:nth-of-type(${index}) > h3`);
+    title.textContent = `Panel ${index}`;
+}
+
+
+// the expand effect using the .active class
+const panels = document.querySelectorAll('.panel');
+
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
